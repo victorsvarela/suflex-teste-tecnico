46fb6cc04cf0eeee179abee10dcc743ce0b4b100
Victor Varela <vitaotapioqueiro@gmail.com>
2022-06-24 00:22:22 -0300

    feat: created querie characterInfo

--- src/Services/queries/index.js
+++ src/Services/queries/index.js
@@ -1,19 +1,36 @@
 import { gql } from "@apollo/client";
 
 export const Filtered = (name, species) => gql`
-  query {
-    characters(filter: { species: ${species}, name: ${name} }) {
-      info {
-        count
-      }
-      results {
-        characters {
-          image
-          name
-          species
-          status
+    query {
+        characters(filter: { species: ${species}, name: ${name} }) {
+            info {
+                count
+            }
+            results {
+                characters {
+                    id
+                    image
+                    name
+                    species
+                    status
+                }
+            }
         }
-      }
     }
-  }
+`;
+
+export const CharacterInfo = (id) => gql`
+    query {
+        charactersByIds(ids: [${id}]) {
+            id
+            name
+            status
+            species
+            image
+            episode {
+                episode
+            }
+            created
+        }
+    }
 `;
